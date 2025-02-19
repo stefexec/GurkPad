a81c5a9b9c750c08382d67cb93b6380023a0bd43
stefexec <stefan.eckel@gmail.com>
2025-02-20 00:33:46 +0100

    window size the easy way

--- main.c
+++ main.c
@@ -1,25 +1,32 @@
 /*  GurkPad - A very simple text editor made using a C tutorial.
  *
- *  Next step in course: 3. Global state
+ *  Next step in course: 3. Window size, the hard way
  *
  *
  */
 
 /*** includes */
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
 
 /*** defines */
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data */
+struct editorConfig {
+  int screenrows;
+  int screencols;
+  struct termios orig_termios;
+};
 
-struct termios orig_termios;
+struct editorConfig E;
 
 /*** terminal */
 
@@ -32,15 +39,15 @@ void die(const char *s) {
 }
 
 void disableRawMode() {
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
 	die("tcsetattr");
 }
 
 void enableRawMode() {
-	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcsetattr");
+	if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcsetattr");
 	atexit(disableRawMode);
 
-	struct termios raw = orig_termios;
+	struct termios raw = E.orig_termios;
 
 	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 	raw.c_oflag &= ~(OPOST);
@@ -62,11 +69,23 @@ char editorReadKey() {
   return c;
 }
 
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  } else { 
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
 /*** output */
 
 void editorDrawRows() {
   int y;
-  for (y = 0; y < 24; y++) {
+  for (y = 0; y < E.screenrows; y++) {
     write(STDOUT_FILENO, "~\r\n", 3);
   }
 }
@@ -96,8 +115,13 @@ void editorProcessKeypress() {
 
 /*** init */
 
+void initEditor() {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
 	enableRawMode();
+  initEditor();
 
 	while (1) {
     editorRefreshScreen();
