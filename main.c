88c863ee890cf1f6b85bdca1693cd8d0828b2050
stefexec <stefan.eckel@gmail.com>
2025-02-22 18:18:54 +0100

    multiple lines

--- main.c
+++ main.c
@@ -7,6 +7,10 @@
 
 /*** includes */
 
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -48,7 +52,7 @@ struct editorConfig {
   int screenrows;
   int screencols;
   int numrows;
-  erow row;
+  erow *row;
   struct termios orig_termios;
 };
 
@@ -169,17 +173,34 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
-/*** file i/o */
+/*** row operations */
 
-void editorOpen() {
-  char *line = "Hello, World!";
-  ssize_t linelen = 13;
-
-  E.row.size = linelen;
-  E.row.chars = malloc(linelen + 1);
-  memcpy(E.row.chars, line, linelen);
+void editorAppendRow(char *s, size_t len) {
+  E.row.size = len;
+  E.row.chars = malloc(len + 1);
+  memcpy(E.row.chars, line, len);
   E.row.chars[linelen] = '\0';
   E.numrows = 1;
+}
+
+/*** file i/o */
+
+void editorOpen(char *filename) {
+  FILE *fp = fopen(filename, "r");
+  if (!fp) die("fopen");
+
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  linelen = getline(&line, &linecap, fp);
+  if (linelen != -1) {
+    while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+      linelen--;
+    editorAppendRow(line, linelen);
+    
+  }
+  free(line);
+  fclose(fp);
 }
 
 /*** append buffer */
@@ -210,7 +231,7 @@ void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
     if (y >= E.numrows) {
-      if (y == E.screenrows / 3) {
+      if (E.numrows == 0 && y == E.screenrows / 3) {
         char welcome[80];
         int welcomelen = snprintf(welcome, sizeof(welcome), "GurkPad editor -- Version %s", GPAD_VERSION);
         if (welcomelen > E.screencols) welcomelen = E.screencols;
@@ -324,14 +345,17 @@ void initEditor() {
   E.cx = 0;
   E.cy = 0;
   E.numrows = 0;
+  E.row = NULL;
 
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
 	enableRawMode();
   initEditor();
-  editorOpen();
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
 
 	while (1) {
     editorRefreshScreen();
