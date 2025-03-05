1ac2d34bec92f3a3ff285e3ac3c261809ea46882
stefexec <stefan.eckel@gmail.com>
2025-03-05 21:18:37 +0100

    added python syntax highlighting

--- syntax.c
+++ syntax.c
@@ -1,25 +1,40 @@
+#include <stdlib.h>
 #include "syntax.h"
-#include <stddef.h>
-
-/*** filetypes */
 
 char *C_HL_extensions[] = { ".c", ".h", ".cpp", NULL };
 char *C_HL_keywords[] = {
-  "switch", "if", "while", "for", "break", "continue", "return", "else",
-  "struct", "union", "typedef", "static", "enum", "class", "case",
+    "switch", "if", "while", "for", "break", "continue", "return", "else",
+    "struct", "union", "typedef", "static", "enum", "class", "case",
+    "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
+    "void|", NULL
+};
 
-  "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
-  "void|", NULL
+char *PY_HL_extensions[] = { ".py", NULL };
+char *PY_HL_keywords[] = {
+    "def", "return", "if", "elif", "else", "for", "while", "break", "continue",
+    "import", "from", "as", "class", "try", "except", "finally", "with", "pass",
+    "raise", "yield", "lambda", "global", "nonlocal", "assert", "del",
+
+    "True|", "False|", "None|", "int|", "float|", "str|", "list|", "dict|",
+    "set|", "tuple|", "bool|", "bytes|", NULL
 };
 
 struct editorSyntax HLDB[] = {
-  {
-    "c",
-    C_HL_extensions,
-    C_HL_keywords,
-    "//", "/*", "*/",
-    HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
-  },
+    {
+        "c",
+        C_HL_extensions,
+        C_HL_keywords,
+        "//", "/*", "*/",
+        HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
+    },
+    {
+        "python",
+        PY_HL_extensions,
+        PY_HL_keywords,
+        "#", NULL, NULL,  // Python has no multi-line comment markers
+        HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
+    }
 };
 
-const unsigned int HLDB_ENTRIES = (sizeof(HLDB) / sizeof(HLDB[0]));
+// Update HLDB_ENTRIES count
+const unsigned int HLDB_ENTRIES = sizeof(HLDB) / sizeof(HLDB[0]);
